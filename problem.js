24492d264b6ef3469f93aa85f000a45311669b5f
Irene <airine@Irenes-MacBook-Air.local>
2020-07-21 00:57:11 -0700

    [problem.js]homework js completed

--- problem.js
+++ problem.js
@@ -44,8 +44,18 @@ console.log(reverseStr('bootcamp'));
 //
 // luckySevens(25); // => [ 7, 14, 21 ]
 // luckySevens(42); // => [ 7, 14, 21, 28, 35, 42 ]
+function luckySevens(max) {
+    let luckyArray = [];
+    for (let i = 0; i < max; i++){
+        if (i % 7 == 0 && i/7 != 0) {
+            luckyArray.push(i);
+        }
+    }
+    return luckyArray
+}
 
-
+console.log(luckySevens(42));
+console.log(luckySevens(25));
 
 
 //### copyMachine
@@ -59,6 +69,18 @@ console.log(reverseStr('bootcamp'));
 // copyMachine('candy', 2); // => [ 'candy', 'candy' ]
 // copyMachine('bread', 4); // => [ 'bread', 'bread', 'bread', 'bread' ]
 // copyMachine(11, 6); // => [ 11, 11, 11, 11, 11, 11 ]
+function copyMachine(element, num) {
+    let copyMachineArray = [];
+    
+    for (i=0; i <element.length; i++) {
+      
+      copyMachineArray.push(element)
+    }
+    return copyMachineArray;
+  } 
+  console.log(copyMachine('bread', 4));
+
+
 
 //### everyOtherWord
 
@@ -69,6 +91,15 @@ console.log(reverseStr('bootcamp'));
 //
 // everyOtherWord('hello how are you doing on this lovely day?'); // => [ 'hello', 'are', 'doing', 'this', 'day?' ]
 // everyOtherWord('the weather is wonderful'); // => [ 'the', 'is' ]
+function everyOtherWord(sentence){
+    const everyOtherWord = [];
+    const sentenceSplit = sentence.split(' ');
+    return sentenceSplit;
+    }
+    
+  
+  console.log(everyOtherWord('hello how are you doing on this lovely day?'));
+
 
 //### wordYeller
 
@@ -85,6 +116,32 @@ console.log(reverseStr('bootcamp'));
 // Example 2
 // let words = "Go to the store and grab the following: milk, bread, run, and cake";
 // wordYeller(words) === "Go! to! the! store! and! grab! the! following: milk, bread, run, and! cake!"; // => true
+function wordYeller(sentence) {
+    var yelledWords = [];
+  
+    var words = sentence.split(" ");
+    // sentence.split(" ") creates array of sentence's words split by spaces.
+     
+    var punctuation = ". , ! ? ; :".split(" "); // array of punctuation
+  
+    for (var i = 0; i < words.length; i++) {
+      var word = words[i];
+      var lastCharacter = word.slice(-1);
+  
+      if (punctuation.indexOf(lastCharacter) !== -1) { // if word already has punctuation
+        yelledWords.push(word);
+      } else {                                         // else the word has no punctuation
+        yelledWords.push(word + "!");
+      }
+    }
+  
+    return yelledWords.join(" ");
+  }
+  
+  
+  console.log(wordYeller("Stop it now! Please, wont you stop?"));
+  console.log(wordYeller("Go to the store and grab the following: milk, bread, run, and cake"));
+
 
 //### arraySubstring
 
@@ -100,15 +157,33 @@ console.log(reverseStr('bootcamp'));
 // arraySubstring(["prefix", "problems", "pragmatic", "prefer"], "pre")
 // => [true, false, false, true]
 
+function arraySubstring(words, str){
+    let array = []
+    for (let i = 0; i < words.length; i++){
+        let word = words[i]
+        if (word.search(str) !== -1){
+            array.push(true)
+        }
+        else {
+            array.push(false)
+        }
+    }
+    return array
+}
+
+console.log(arraySubstring(["hello", "history", "helix", "hellos"],"hel"));
+console.log(arraySubstring(["prefix", "problems", "pragmatic", "prefer"], "pre"));
+
 //### evenCaps
 
-// Write a function evenCaps(sentence) that takes in a sentence as an
-// argument and returns the same string with each even index being
-// capitalized. Index 0 counts as even.
-//
-// Examples:
-//
-// evenCaps("Tom got a small piece of pie")
-// => "ToM GoT A SmAlL PiEcE Of pIe"
-// evenCaps("the book is in front of the table")
-// => "ThE BoOk iS In fRoNt oF ThE TaBlE"
+
+function evenCaps(sentence){
+    return sentence.split(' ').map(word => //split each word
+      //split each letter, use modulus on the word instead of sentence to make lowerCase or upperCase, then join words together back to sentence
+      word.split('').map((itm, idx) => idx % 2 ?  itm.toLowerCase() : itm.toUpperCase()).join('')
+    ).join(' ')
+  }
+  
+  console.log(evenCaps('Tom got a small piece of pie'));
+  console.log(evenCaps('the book is in front of the table'));
+  
